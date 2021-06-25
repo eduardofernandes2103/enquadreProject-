b8d60cad71c90dfd0e19abfc0b022abf35e4744f
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-06-25 20:27:38 -0300

    feat: Create GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AuthProvider } from "./providers/auth";
 import Routes from './Routes';
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <AuthProvider>
       <Routes />
+      <GlobalStyle />
     </AuthProvider>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: 0;
+  font-family: roboto;
+ }
+
+ body {
+    background: #4E4A4A;
+    color: white;
+    -webkit-font-smoothing: antialiased !important
+  }
+
+  body, input, button {
+    font-family: 'Nunito', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+    width: 9rem;
+    height: 2rem;
+    background-color: #7B7979;
+    color: black;
+    border: 0.5px solid #7B7979;
+    border-radius: 5px;
+    margin: 5px;
+    font-weight: bold;
+    font-size: 0.8rem;
+  }
+
+  button:hover {
+    box-shadow: 1px 1px 1px rgba(0,0,0,0.3);
+    transition: 0.2s;
+    background-color: gray;
+    color: black;
+  }
+  
+  a {
+    text-decoration: none;
+    color: #7B7979;
+  }
+`;
