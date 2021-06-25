163dd1edbee8c54b45cde0db7a9f8fb39e7841eb
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-06-25 20:01:36 -0300

    feat: Create Home structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
+
 const Home = () =>{
-    return <div>Home</div>
+    return (
+                <div>
+                    
+                    <div className="placeHome">
+                        <h1>Enquadre</h1>
+                        <h2>Transformando sua casa em Arte</h2>
+                    </div>
+
+                    <div className="buttonPlace">
+                        <button>Login</button>
+                        <button>Signup</button>
+                    </div>
+
+                    <div className="redirectPlace">
+                        <Link to='/store'> Ir direto Às Compras</Link>
+                    </div>
+                </div>
+    )
 }
 
 export default Home
