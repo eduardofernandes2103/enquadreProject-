a39c391c4d517a4281f4c5e3448d6e10ac43154c
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-06-27 13:41:52 -0300

    feat: create Store page

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -1,29 +1,41 @@
 import { useProducts } from '../../providers/products'
 import CardOfProduct from '../../components/CardOfProduct'
-import { Container } from './styles'
+import { Container, CartPlace } from './styles'
+import { FaShoppingCart } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
 
 const Store = () =>{
-    
+
     const {products} = useProducts();
     
     return (
-        <Container>
-            <h2>Bem vindo!</h2>
-            <h3>Deixe a Arte entrar na sua casa</h3>
+        <div>
+        
+            <CartPlace>
+                <Link to="/cart"><div className="icon"><FaShoppingCart /></div></Link>
+            </CartPlace>
 
-            {products.map((product)=>(
-                            
-                            <CardOfProduct 
-                                id={product.id} 
-                                title={product.title}
-                                image={product.image}
-                                price={product.price}
-                                children="Comprar"
-                            />
+            <Container>
+            
+                <h2>Bem vindo!</h2>
+                <h3>Deixe a Arte entrar na sua casa</h3>
 
-                        ))}
+                {products.map((product)=>(
+                                
+                                <CardOfProduct 
+                                    id={product.id} 
+                                    title={product.title}
+                                    image={product.image}
+                                    price={product.price}
+                                    children="Comprar"
+                                />
 
-        </Container>
+                            ))}
+
+            </Container>
+
+        </div>
     )
 }
 
--- src/pages/Store/styles.ts
+++ src/pages/Store/styles.ts
@@ -10,12 +10,26 @@ export const Container = styled.div`
     margin: 2rem;
     text-align: center;
 
-    h2{
-        margin: 2.3rem;
+    h2{ 
+        margin-bottom: 2rem;
         font-size: 3rem;
     }
 
     h3{
         margin: 1rem;
     }
+`
+export const CartPlace = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    margin-top: 1rem;
+    margin-right: 1rem;
+    font-size: 2rem;
+    color: #00cc00;
+    
+    .icon{
+        color: #00cc00;
+    }
+    
 `
