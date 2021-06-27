613db0a5603235a05f297fc8d48ba29964405949
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-06-27 09:41:33 -0300

    feat: Style of Login page

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,48 +1,48 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Container = styled.div`
+export const Container = styled.div`
   
-//     display: flex;
-//     flex-direction: column;
-//     flex-wrap: wrap;
-//     align-items: center;
-//     justify-content: center;
-//     margin: 2rem;
-//     text-align: center;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem;
+    text-align: center;
 
-//     h2{
-//         margin: 3rem;
-//         font-size: 4rem;
-//     }
+    h2{
+        margin: 3rem;
+        font-size: 4rem;
+    }
 
-//     .inputPlace{
+    .inputPlace{
 
-//         display: flex;
-//         flex-direction: column;
+        display: flex;
+        flex-direction: column;
 
-//         input{
-//             margin: 0.7rem;
-//             border-radius: 5px;
-//             border: none;
-//             width: 15rem;
-//             height: 1.9rem;
-//             background: #7B7979;
-//             color: white;
-//             pading: 1.5rem;
-//             ::placeholder{
-//                 color: #373535;
-//                 padding: 1rem;
-//             }
-//         }
+        input{
+            margin: 0.7rem;
+            border-radius: 5px;
+            border: none;
+            width: 15rem;
+            height: 1.9rem;
+            background: #7B7979;
+            color: white;
+            pading: 1.5rem;
+            ::placeholder{
+                color: #373535;
+                padding: 1rem;
+            }
+        }
 
-//         span{
-//             color: red;
-//             font-size: 0.8rem;
-//         }
-//     }
+        span{
+            color: red;
+            font-size: 0.8rem;
+        }
+    }
 
-//     button{
-//         margin: 1rem;
-//     }
+    button{
+        margin: 1rem;
+    }
 
-// `
+`
